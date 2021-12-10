750a846424eedf2b831e1432eaa960992c01d292
dmitrushagood <93004624+dmitrushagood@users.noreply.github.com>
2021-12-10 21:52:33 +0300

    Delete wew.py

--- wew.py
+++
@@ -1,9 +0,0 @@
-import requests
-import json
-keys = {
-    'bitcoin':'AED',
-    'aff':'GTQ',
-    'dollar':'INR'
-}
-res = requests.get(f'https://api.fastforex.io/fetch-one?from=AED&to=GTQ&api_key=a8c5d78b71-4bb800ad36-r3wgyn')
-print(json.loads(res.content)['result'][keys[base]])
